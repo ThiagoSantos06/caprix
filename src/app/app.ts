b59bf4f665bd67b4f55744b7db1563f80ed0a9c1
ThiagoSantos_1 <thiagohenrique1983c@gmail.com>
2026-02-19 13:13:21 -0300

    Configurações do projeto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,15 @@
 import { Component, signal } from '@angular/core';
+import { Header } from "./Shared/Components/header/header";
+import { Hero } from "./Pages/hero/hero";
+import { Feature } from "./Pages/feature/feature";
+import { AboutMe } from "./Pages/about-me/about-me";
+import { Contact } from "./Pages/contact/contact";
+import { Location } from "./Pages/location/location";
+import { Footer } from "./Shared/Components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Header, Hero, Feature, AboutMe, Contact, Location, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
