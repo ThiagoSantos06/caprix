71b47f657aa30b36e184c0c0728c32881728494f
ThiagoSantos_1 <thiagohenrique1983c@gmail.com>
2026-02-24 13:44:40 -0300

    Atualiza contatos

--- src/app/Pages/contact/contact.ts
+++ src/app/Pages/contact/contact.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './contact.scss',
 })
 export class Contact {
-  readonly whatsappUrl = 'https://wa.me/5521996279623?text=Olá%2C%20meu%20nome%20%C3%A9%20%5BSeu%20nome%5D.%20Quero%20solicitar%20um%20or%C3%A7amento%21';
+  readonly whatsappUrl = 'https://wa.me/5521970557657?text=Olá%2C%20meu%20nome%20%C3%A9%20%5BSeu%20nome%5D.%20Quero%20solicitar%20um%20or%C3%A7amento%21';
 
   openWhatsApp() {
     window.open(this.whatsappUrl, '_blank');
